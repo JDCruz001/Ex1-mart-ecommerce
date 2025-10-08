ddb01c0e48807918b2db2bebac831032ab83b677
Juan Cruz <138948046+JDCruz001@users.noreply.github.com>
2025-10-08 13:29:27 -0500

    Add files via upload
    git add.
    git commit  -m "feat: initial setup with product service"
    git push origin main

---
+++ producto-service.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app= express();
+app.use(express.json()); 
+
+1et productos = [
+    { id: 1, nombre: "Psp", precio:590, stock:10},
+    { id: 2, nombre: "Teclado inalambrico", precio:250, stock:20}
+];
+
+app.get('/productos',(req, res)=> {
+    res.json(productos);
+});
+
+app.listen(3001, ()=> {
+    console.log('Servicios Productos ejecutandose en puerto 3001');
+});
